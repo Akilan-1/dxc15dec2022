13c2353a24b140b40c6a5170205e8629ea50df0e
Akilan-1 <120648347+Akilan-1@users.noreply.github.com>
2022-12-15 17:38:32 +0530

    Add files via upload
    windows script

---
+++ script.bat
@@ -0,0 +1,19 @@
+
+cd ..\..
+mkdir powershell
+cd powershell
+mkdir dir1
+mkdir dir2
+cd dir1
+mkdir dir11
+mkdir dir12
+cd dir11
+type nul>f1.txt
+type nul>f2.txt
+type nul>f1.csv
+type nul>f2.csv
+cd ..\..\dir2
+mkdir dir21
+mkdir dir22
+cd dir21
+mkdir dir211
---
+++ script2.bat
@@ -0,0 +1,7 @@
+cd\
+cd powershell\dir1\dir11
+copy *.txt ..\..\dir2\dir21
+copy *.csv ..\..\dir2\dir22
+copy f1.txt ..\..\dir2\dir21\dir211
+cd ..
+rmdir dir11/s
